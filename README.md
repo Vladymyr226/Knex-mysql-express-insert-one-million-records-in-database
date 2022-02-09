d5c78bc5f06923ec2cfad91a89f4650ac555974f
Vladymyr Svyrydenko <62399426+Vladymyr226@users.noreply.github.com>
2022-02-09 12:20:48 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
-# Knex-mysql-express-insert-one-million-records-in-database
-Connect DB with your backend (NodeJs),
-create insert script, create 1 million records in the tables
+# Connect DB with your backend (NodeJs)
+# Create insert script, create 1 million records in the tables
---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const db = require("./db/db.js");
+const faker = require('faker'); //https://github.com/faker-js/faker, version 5.5.0 work, more - no.
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const SERVER_PORT = 3000;
+
+
+// app.post("/", async (req, res) => {
+//   res.send("HI, Volodya!!!");
+
+//   for (let index = 0; index < 10000; index++) {
+//     await db('Ceil').insert({ Price: faker.commerce.price(), Quantity: faker.datatype.number(), Units: faker.finance.currencySymbol() })
+//   }
+//   console.log("/ done");
+// })
+
+
+app.post("/batch", async (req, res) => {
+  const rows = [];
+  const chunkSize = 1000000;
+
+  for (let index = 0; index < chunkSize; index++) {
+    rows.push({ Price: faker.commerce.price(), Quantity: faker.datatype.number(), Units: faker.finance.currencySymbol() }
+    )
+  }
+  db.batchInsert('Ceil', rows, chunkSize)
+    .returning('id')
+    .then(function (ids) { console.log("Yes"); })
+    .catch(function (error) { console.log("No") });
+});
+
+app.listen(SERVER_PORT, () => {
+  console.log(`Example app listening at http://localhost:${SERVER_PORT}`)
+});
